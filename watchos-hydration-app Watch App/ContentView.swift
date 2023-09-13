f941b0d1f491147dc5c65c10a9da7ed7d67f2571
Masao Kitamura <masaok@gmail.com>
2023-09-13 01:31:31 -0700

    Hydration app works

--- watchos-hydration-app Watch App/ContentView.swift
+++ watchos-hydration-app Watch App/ContentView.swift
@@ -5,12 +5,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var waterIntake: Double = 0
+    let dailyGoal: Double = 2000 // in ml, e.g., 2 liters
+    
+    var clampedWaterIntake: Double {
+        return min(max(waterIntake, 0), dailyGoal)
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 16) {
+            Text("Hydration Reminder")
+                .font(.headline)
+            
+            Text("\(Int(clampedWaterIntake))ml / \(Int(dailyGoal))ml")
+                .font(.title)
+            
+            ProgressView(value: clampedWaterIntake, total: dailyGoal)
+            
+            HStack {
+                Button(action: {
+                    waterIntake += 250
+                }) {
+                    Text("+250ml (Glass)")
+                }
+                
+                Button(action: {
+                    waterIntake += 500
+                }) {
+                    Text("+500ml (Bottle)")
+                }
+            }
         }
         .padding()
     }
