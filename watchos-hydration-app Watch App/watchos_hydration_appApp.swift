7502457c6846c9f197541cfbbc3e67d6d75cf392
Masao Kitamura <masaok@gmail.com>
2023-09-13 01:24:13 -0700

    Initial Commit

---
+++ watchos-hydration-app Watch App/watchos_hydration_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchos_hydration_appApp.swift
+//  watchos-hydration-app Watch App
+//
+//  Created by keckadmin on 9/13/23.
+//
+
+import SwiftUI
+
+@main
+struct watchos_hydration_app_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
